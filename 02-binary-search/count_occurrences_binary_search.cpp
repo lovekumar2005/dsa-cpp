a77ebe7ae76ff03565563539a82e27b4d18aed61
Love Kumar <lovepehlaj2005@gmail.com>
2026-08-09 22:02:17 +0500

    feat: add new binary search problem

---
+++ 02-binary-search/count_occurrences_binary_search.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int leftOccurnece(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start)/2;
+        if(arr[mid] == key){
+            ans = mid;
+            end = mid - 1;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int rightOccurnece(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+    int ans = -1;
+
+    while (start <= end)
+    {
+        int mid = start + (end - start)/2;
+        if(arr[mid] == key){
+            ans = mid;
+            start = mid + 1;
+        }
+        else if(key > arr[mid]){
+            start = mid + 1;
+        }else{
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int size = 10;
+    int arr[size] = {0, 1, 1, 3, 5, 6, 7, 7, 7, 9};
+
+    cout << "Total occurence of 9 is: " << (rightOccurnece(arr, size, 9) - leftOccurnece(arr, size, 9)) + 1 << endl;
+
+   return 0;
+}
