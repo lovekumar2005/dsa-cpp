11e1c319be255c15df8c551cc1cd210adf646f89
Love Kumar <lovepehlaj2005@gmail.com>
2026-08-11 23:00:07 +0500

    feat: add square root with precision

---
+++ 02-binary-search/square_root_with_precision.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+int squareRoot(int num){
+    int start = 0;
+    int end = num;
+    long long int ans = 0;
+
+    while(start <= end){
+        long long int mid = start + (end - start) / 2;
+        long long int square = mid * mid;
+        if(square == num){
+            return mid;
+        }
+        else if(square < num){
+            ans = mid;
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+double squareRootWithPrecision(int num, int precision, int tempSol){
+    double ans = tempSol;
+    double factor = 1;
+
+    for(int i = 0; i < precision; i++){
+        factor = factor / 10;
+        for(double j = ans; j * j < num; j = j +factor){
+            ans = j;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int num = 26;
+
+    int tempSol = squareRoot(num);
+
+    cout << "Square root of " << num << " is: " << squareRootWithPrecision(num, 3, tempSol) << endl;
+ 
+   return 0;
+}
