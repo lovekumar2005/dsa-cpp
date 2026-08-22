9c66ec816fcd3b491ff6cc1bfc8b790a9c4d17f4
Love Kumar <lovepehlaj2005@gmail.com>
2026-08-22 21:06:52 +0500

    feat: add bubble sort

--- 03-sorting/bubble_sort.cpp
+++ 03-sorting/bubble_sort.cpp
@@ -3,10 +3,15 @@ using namespace std;
 
 void sortArray(int arr[], int size){
     for(int i = 0; i < size - 1; i++){
+        bool swapped = false;
         for(int j = 0; j < size - 1 - i; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                swapped = true;
             }
+        }
+        if(swapped == false){
+            break;
         }
     }
 }
